7483346545d24e851b72ce7cee3860794b7db444
akantuni <akantuni@gmail.com>
2018-10-17 10:28:51 -0400

    Add .gitignore

---
+++ sevenwonders.py
@@ -0,0 +1,29 @@
+x = input()
+
+nT = 0
+nC = 0
+nG = 0
+
+xf = list(x)
+
+for i in (xf):
+    if i == "T":
+        nT += 1
+
+for i in (xf):
+    if i == "G":
+        nG += 1
+
+for i in (xf):
+    if i == "C":
+        nC += 1
+
+points = nT ** 2 + nC ** 2 + nG ** 2
+
+if "T" in xf and "G" in xf and "C" in xf:
+    if min(nT, nG, nC) == 1:
+        points += 7
+    elif min(nT, nG, nC) > 1:
+        points += (min(nT, nG, nC) * 7)
+
+print(points)
