38f215a9cea9efa59cb5e8adf5a9a55eacf08e80
akantuni <akantuni@gmail.com>
2018-10-17 21:36:36 -0400

    Add datum.py

---
+++ datum.py
@@ -0,0 +1,9 @@
+import datetime
+n = input()
+d = int(n.split()[0])
+m = int(n.split()[1])
+
+days = {0: "Monday", 1: "Tuesday", 2: "Wednesday", 3: "Thursday", 4: "Friday", 5: "Saturday", 6: "Sunday"}
+day = datetime.date(2009, m, d)
+
+print(days.get(day.weekday()))
