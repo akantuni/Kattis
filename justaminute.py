ca562cf70e8d12b2cd753ce70710906c1701ea36
akantuni <akantuni@gmail.com>
2018-11-02 23:01:26 -0400

    Add justaminute.py

---
+++ justaminute.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+sec = 0
+min = 0
+
+for i in range(n):
+    s = input()
+    min += int(s.split()[0])
+    sec += int(s.split()[1])
+
+sl = sec / 60 / min
+
+if sl > 1:
+    print(sl)
+else:
+    print('measurement error')
