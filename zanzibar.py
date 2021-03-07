b8c1b09057ca688f0b75e01fe4d30c69e9143f8e
Alex Kantuni <akantuni@gmail.com>
2021-03-06 19:02:09 -0500

    Var renaming

--- zanzibar.py
+++ zanzibar.py
@@ -2,13 +2,13 @@ n = int(input())
 
 for i in range(n):
     array = list(map(int, input().split()))
-    sum = 0
+    sm = 0
     counter = 0
     for number in array:
         num2 = array[counter + 1]
         if number == 0 or num2 == 0:
             break
         elif num2 > number * 2:
-            sum += num2 - number * 2
+            sm += num2 - number * 2
         counter += 1
-    print(sum)
+    print(sm)
